1236cc5b253e1df79c7cb4c947b053665342ce6f
Zartab Nakhwa <zartab@codewithz.com>
2021-11-30 13:46:59 +0530

    Initial Commit

---
+++ app/screens/WelcomeScreen.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, Text, View, ImageBackground } from 'react-native'
+
+export default function WelcomeScreen() {
+    return (
+        <ImageBackground
+            style={styles.background}
+            source={require('../assets/background.jpg')}
+
+        >
+
+
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+
+    },
+
+})
