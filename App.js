46b206c27a421f3157a40f3b5a9e3b72f865f2c0
Zartab Nakhwa <zartab@codewithz.com>
2021-11-30 14:30:57 +0530

    View Image Screen

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
