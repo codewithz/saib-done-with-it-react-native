332585dd4b3e0bb9d79a0a3b7ee49105f8587eb8
Zartab Nakhwa <zartab@codewithz.com>
2021-12-01 14:15:32 +0530

    Resubale Icon COmponent

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 
@@ -8,19 +8,23 @@ import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessageScreen from './app/screens/MessageScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App() {
   return (
-    <MessageScreen />
+    <Screen>
+      <Icon
+        name="email"
+        size={60}
+        backgroundColor="greenyellow"
+        iconColor="white"
+
+      />
+    </Screen>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#f8f4f4',
-    padding: 20,
-    paddingTop: 100
-  }
-})
+
 
 
