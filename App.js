45f27a0ea76090ca94800359da4b814237f06169
Zartab Nakhwa <zartab@codewithz.com>
2021-12-02 17:19:51 +0530

    AppNavigator

--- App.js
+++ App.js
@@ -24,13 +24,14 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import AuthNavigator from './app/navigation/AuthNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
+import AppNavigator from './app/navigation/AppNavigator';
 
 
 export default function App() {
 
   return (
     <NavigationContainer theme={navigationTheme}>
-      <AuthNavigator />
+      <AppNavigator />
     </NavigationContainer>
   );
 }
