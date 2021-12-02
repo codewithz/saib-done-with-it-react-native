8548560bba102b6042e176573ac8c7df119377b4
Zartab Nakhwa <zartab@codewithz.com>
2021-12-02 12:44:20 +0530

    AppForm Implementation

--- app/screens/LoginScreen.jsx
+++ app/screens/LoginScreen.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
-import { Formik } from 'formik';
+import { StyleSheet, Image } from 'react-native'
+
 import * as Yup from 'yup';
 
 
 import Screen from './../components/Screen';
-import AppTextInput from '../components/AppTextInput';
-import AppButton from '../components/AppButton';
-import AppText from './../components/AppText';
-import ErrorMessage from '../components/ErrorMessage';
 import AppFormField from '../components/AppFormField';
 import SubmitButton from '../components/SubmitButton';
+import AppForm from '../components/AppForm';
+
 
 const validationSchema = Yup.object().shape({
     username: Yup.string().required().email().label('Email'),
@@ -27,41 +25,35 @@ export default function LoginScreen(props) {
                 source={require('../assets/logo-red.png')}
             />
 
-            <Formik
+            <AppForm
                 initialValues={{ username: '', password: '' }}
                 onSubmit={values => console.log(values)}
                 validationSchema={validationSchema}
-
             >
-                {() =>
 
-                (
-                    <React.Fragment>
-                        <AppFormField
-                            icon="email"
-                            keyboardType="email-address"
-                            placeholder='Email'
-                            textContentType="emailAddress"
-                            autoCorrect={false}
-                            autoCapitalize="none"
-                            name="username"
-                        />
-                        <AppFormField
-                            icon="lock"
-                            secureTextEntry={true}
-                            placeholder='Password'
-                            textContentType="password"
-                            autoCorrect={false}
-                            autoCapitalize="none"
-                            name="password"
-                        />
+                <AppFormField
+                    icon="email"
+                    keyboardType="email-address"
+                    placeholder='Email'
+                    textContentType="emailAddress"
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    name="username"
+                />
+                <AppFormField
+                    icon="lock"
+                    secureTextEntry={true}
+                    placeholder='Password'
+                    textContentType="password"
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    name="password"
+                />
 
-                        <SubmitButton title="Login" />
-                    </React.Fragment>
-                )
+                <SubmitButton title="Login" />
 
-                }
-            </Formik>
+
+            </AppForm>
 
 
 
